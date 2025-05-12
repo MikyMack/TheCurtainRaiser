97f265444ad44c29312f7216b15a30b81992d98a
micky mack <jijomonjs@gmail.com>
2025-05-12 22:41:28 +0530

    updated

--- backend/models/Gallery.js
+++ backend/models/Gallery.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const gallerySchema = new mongoose.Schema({
-  title: { type: String, required: true },
+  title: { type: String, required: false },
   imageUrl: { type: String, required: true },
   category: { type: String, required: true },
   createdAt: { type: Date, default: Date.now }
